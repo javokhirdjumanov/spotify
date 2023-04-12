0dff934e95f3f16bfad2a1d88e077fb8c9945420
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-12 17:13:59 +0500

    add DI container

---
+++ src/ecommerce.api/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace ecommerce.api.Extensions;
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApis(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
