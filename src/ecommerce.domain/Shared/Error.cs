e8925ecdda98d9dfa4bf8d631625ead77cbc72a8
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-12 18:39:12 +0500

    fixed the error

--- src/ecommerce.domain/Shared/Error.cs
+++ src/ecommerce.domain/Shared/Error.cs
@@ -3,6 +3,8 @@
 namespace ecommerce.domain.Shared;
 public class Error : IEquatable<Error>
 {
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new Error("Error.NullValue", "The specified result value is null.");
     public Error(string code, string message)
     {
         this.code = code;
@@ -12,6 +14,7 @@ public class Error : IEquatable<Error>
     public string message { get; }
 
     public static implicit operator string(Error error) => error.code;
+
     public bool Equals(Error? other)
     {
         if(other is null)
