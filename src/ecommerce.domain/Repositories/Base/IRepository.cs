b37a16ed0a2b04c0909681729a8995b9bc03dfba
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-12 15:23:57 +0500

    add base repository

---
+++ src/ecommerce.domain/Repositories/Base/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace ecommerce.domain.Repositories.Base
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IReadOnlyList<T>> SelectAllAsync(
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<T> SelectAsync(
+            CancellationToken cancellationToken = default, 
+            params object[] entityIds);
+
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
