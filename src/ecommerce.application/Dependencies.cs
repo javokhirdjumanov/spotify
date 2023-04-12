1fbaaaa347a4e18b979cb68ab85e94c7f96e89f7
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-12 17:33:30 +0500

    fixed

---
+++ src/ecommerce.application/Dependencies.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ecommerce.application;
+public static class Dependencies
+{
+    public static IServiceCollection AddAplication(this IServiceCollection services)
+    {
+        services.AddMediatR(typeof(Dependencies).Assembly);
+
+        return services;
+    }
+}
