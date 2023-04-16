4b5654c71fa32ef8f384a2ee441d84017e2f05a8
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-17 00:43:06 +0500

    add

--- src/ecommerce.application/Applications/Messaging/IQuery.cs
+++ src/ecommerce.application/Applications/Messaging/IQuery.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using ecommerce.domain.Shared;
 
 namespace ecommerce.application.Applications.Messaging;
 public interface IQuery<TResponse> : IRequest<Result<TResponse>>
---
+++ src/ecommerce.application/Applications/Messaging/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using ecommerce.domain.Shared;
+using MediatR;
+
+namespace ecommerce.application.Applications.Messaging;
+public interface IQueryHandler<TQuery, TResponse>
+    : IRequestHandler<TQuery, Result<TResponse>>
+    where TQuery : IQuery<TResponse>
+{
+
+}
