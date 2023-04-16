cc4479b5e7c6f2a4dd5bf2b6e55fda674bd7e8f4
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-16 23:55:17 +0500

    add

--- src/ecommerce.domain/Shared/ResultT.cs
+++ src/ecommerce.domain/Shared/ResultT.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ecommerce.domain.Shared
+﻿namespace ecommerce.domain.Shared;
+public class Result<TValue> : Result
 {
-    internal class ResultT
-    {
-    }
+    private readonly TValue? _value;
+
+    protected internal Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error) =>
+        _value = value;
+
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can not be accessed.");
+
+    public static implicit operator Result<TValue>(TValue? value) => Create(value);
 }
