8219e31a4a96625d32a0c13a2a59d2f6307a7639
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-17 01:39:28 +0500

    add

--- src/ecommerce.application/Dependencies.cs
+++ src/ecommerce.application/Dependencies.cs
@@ -1,12 +1,18 @@
-﻿using MediatR;
+﻿using ecommerce.application.Mappers;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
 
 namespace ecommerce.application;
 public static class Dependencies
 {
-    public static IServiceCollection AddAplication(this IServiceCollection services)
+    public static IServiceCollection AddAplication(
+        this IServiceCollection services)
     {
         services.AddMediatR(typeof(Dependencies).Assembly);
+
+        services.AddAutoMapper(typeof(MappProfile));
 
         return services;
     }
