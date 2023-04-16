40d9dd526b8f6f5b192ffd69104f6c5d62627932
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-17 01:39:22 +0500

    add auto mapper

--- src/ecommerce.application/Mappers/MappProfile.cs
+++ src/ecommerce.application/Mappers/MappProfile.cs
@@ -1,7 +1,11 @@
 ﻿using AutoMapper;
+using ecommerce.domain.Entities;
 
 namespace ecommerce.application.Mappers;
 public class MappProfile : Profile
 {
-    
+    public MappProfile()
+    {
+        //CreateMap<CreateUserCommand, Users>();
+    }
 }
