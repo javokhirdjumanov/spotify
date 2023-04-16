6ce27026a0ec66edaf8e042e731ef14c04a7fe21
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-16 23:55:09 +0500

    add

---
+++ src/ecommerce.domain/Shared/IValidationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace ecommerce.domain.Shared;
+public interface IValidationResult
+{
+    public static readonly Error ValidationError = new(
+    "ValidationError",
+    "A validation problem occurred.");
+
+    Error[] Errors { get; }
+}
