ee39bcc931cc1754ac3ffdb54ccc95e6b39a7b9f
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-17 00:46:45 +0500

    add ICommand interface

---
+++ src/ecommerce.application/Applications/Messaging/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using ecommerce.domain.Shared;
+using MediatR;
+
+namespace ecommerce.application.Applications.Messaging;
+public interface ICommand : IRequest<Result>
+{
+}
+
+public interface ICommand<TRespose> : IRequest<Result<TRespose>>
+{
+}
