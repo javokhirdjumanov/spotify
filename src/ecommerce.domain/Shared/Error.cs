71c3510f77d67260ae0d56a876405668c2a74714
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-16 23:43:14 +0500

    add error class

--- src/ecommerce.domain/Shared/Error.cs
+++ src/ecommerce.domain/Shared/Error.cs
@@ -4,25 +4,48 @@ namespace ecommerce.domain.Shared;
 public class Error : IEquatable<Error>
 {
     public static readonly Error None = new(string.Empty, string.Empty);
-    public static readonly Error NullValue = new Error("Error.NullValue", "The specified result value is null.");
+    public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null.");
+
     public Error(string code, string message)
     {
-        this.code = code;
-        this.message = message;
+        Code = code;
+        Message = message;
     }
-    public string code { get; }
-    public string message { get; }
+    public string Code { get; }
+    public string Message { get; }
 
-    public static implicit operator string(Error error) => error.code;
+    public static implicit operator string(Error error) => error.Code;
 
-    public bool Equals(Error? other)
+    public static bool operator ==(Error? a, Error? b)
     {
-        if(other is null)
+        if (a is null && b is null)
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
         {
             return false;
         }
 
-        return 
-            this.code == other.code && message == other.message;
+        return a.Equals(b);
     }
+
+    public static bool operator !=(Error? a, Error? b) => !(a == b);
+
+    public virtual bool Equals(Error? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return Code == other.Code && Message == other.Message;
+    }
+
+    public override bool Equals(object? obj) => obj is Error error && Equals(error);
+
+    public override int GetHashCode() => HashCode.Combine(Code, Message);
+
+    public override string ToString() => Code;
 }
