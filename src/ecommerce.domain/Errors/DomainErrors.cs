6d958cf14493920123a7b8d00028201e1c322f1b
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-23 19:34:39 +0500

    add Domain Error

---
+++ src/ecommerce.domain/Errors/DomainErrors.cs
@@ -0,0 +1,18 @@
+﻿using ecommerce.domain.Shared;
+
+namespace ecommerce.domain.Errors;
+public static class DomainErrors
+{
+    public static class Users
+    {
+        public static readonly Func<Guid, Error> NotFound = id => new Error(
+            code: "User.NotFound",
+            message: $"The user with the identifier {id} was not found."); 
+    }
+    public static class Address
+    {
+        public static readonly Func<Guid, Error> NotFound = id => new Error(
+            code: "Address.NotFound",
+            message: $"The address with the identifier {id} was not found.");
+    }
+}
