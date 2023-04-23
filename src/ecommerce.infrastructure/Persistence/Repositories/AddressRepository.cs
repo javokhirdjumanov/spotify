e7a331b34f402c804b80ccf97dee47f3ff0754f8
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-23 14:23:09 +0500

    add

--- src/ecommerce.infrastructure/Persistence/Repositories/AddressRepository.cs
+++ src/ecommerce.infrastructure/Persistence/Repositories/AddressRepository.cs
@@ -4,7 +4,7 @@ using ecommerce.domain.Repositories;
 namespace ecommerce.infrastructure.Persistence.Repositories;
 public class AddressRepository : Repository<Address>, IAddressRepository
 {
-    private readonly ApplicationDBContext context;
-    public AddressRepository(ApplicationDBContext context) : base(context)
-        => this.context = context;
+    public AddressRepository(ApplicationDBContext context) 
+        : base(context)
+    { }
 }
