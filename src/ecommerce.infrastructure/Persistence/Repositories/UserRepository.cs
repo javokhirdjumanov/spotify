d1a08a8d859512da92a322e49857881c2b192497
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-23 14:23:16 +0500

    add

--- src/ecommerce.infrastructure/Persistence/Repositories/UserRepository.cs
+++ src/ecommerce.infrastructure/Persistence/Repositories/UserRepository.cs
@@ -4,7 +4,7 @@ using ecommerce.domain.Repositories;
 namespace ecommerce.infrastructure.Persistence.Repositories;
 public class UserRepository : Repository<Users>, IUserRepository
 {
-    private readonly ApplicationDBContext context;
-    public UserRepository(ApplicationDBContext context) : base(context)
-        => this.context = context;
+    public UserRepository(ApplicationDBContext context) 
+        : base(context)
+    { }
 }
