e6ad44e5c2b71074c5ca668eee24f89cf1e6fa86
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-23 20:19:06 +0500

    add user controller

---
+++ src/ecommerce.api/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using ecommerce.application.Users.GetUserById;
+using ecommerce.domain.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerce.api.Controllers;
+[Route("api/[controller]")]
+public class UserController : ApiController
+{
+    public UserController(ISender sender) 
+        : base(sender)
+    { }
+
+    [HttpGet("{id:guid}")]
+    public async ValueTask<IActionResult> GetUserById(
+        Guid id, CancellationToken cancellationToken)
+    {
+        var query = new Query(id: id);
+
+        Result<Response> response = await this.sender.Send(query, cancellationToken);
+
+        return response.IsSuccess 
+            ? Ok(response) : BadRequest(response);
+    }
+}
