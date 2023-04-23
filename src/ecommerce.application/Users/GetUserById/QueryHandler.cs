1837cdfa1519b8841fb9b14abe8edaab720aa487
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-23 20:18:20 +0500

    Implementation

--- src/ecommerce.application/Users/GetUserById/QueryHandler.cs
+++ src/ecommerce.application/Users/GetUserById/QueryHandler.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ecommerce.application.Applications.Messaging;
+using ecommerce.domain.Entities;
+using ecommerce.domain.Errors;
+using ecommerce.domain.Repositories;
+using ecommerce.domain.Shared;
 
-namespace ecommerce.application.Users.GetUserById
+namespace ecommerce.application.Users.GetUserById;
+public class QueryHandler : IQueryHandler<Query, Response>
 {
-    internal class QueryHandler
+    private readonly IUserRepository userRepository;
+    public QueryHandler(IUserRepository userRepository)
+        => this.userRepository = userRepository;
+
+    public async Task<Result<Response>> Handle(
+        Query request,
+        CancellationToken cancellationToken)
     {
+        User? isUser = await this.userRepository.SelectAsync(
+            entityIds: request.id,
+            cancellationToken: cancellationToken); 
+
+        if(isUser == null)
+        {
+            return Result.Failure<Response>(
+                DomainErrors.Users.NotFound(request.id));
+        }
+
+        var response = new Response(
+            isUser.Id,
+            isUser.Fullname,
+            isUser.Email,
+            isUser.Username,
+            isUser.Phonenumber,
+            isUser.Password,
+            ((int)isUser.Role),
+            isUser.AddressId);
+
+        return response;
     }
 }
--- src/ecommerce.application/Users/GetUserById/Response.cs
+++ src/ecommerce.application/Users/GetUserById/Response.cs
@@ -1,2 +1,12 @@
-﻿namespace ecommerce.application.Users.GetUserById;
-public record Response();
+﻿using ecommerce.domain.Enums;
+
+namespace ecommerce.application.Users.GetUserById;
+public record Response(
+    Guid id,
+    string Fullname,
+    string Email,
+    string Username,
+    string Phonenumber,
+    string Password,
+    int Role,
+    Guid addressId);
