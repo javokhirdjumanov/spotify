17ff1a392c616343de4f2408d191a9b444d2d5fd
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-05-14 14:50:55 +0500

    add new endpoint that user delete

---
+++ src/ecommerce.application/Users/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,5 @@
+﻿using ecommerce.application.Applications.Messaging;
+
+namespace ecommerce.application.Users.DeleteUser;
+public record DeleteUserCommand(Guid id) 
+    : ICommand<Guid>;
---
+++ src/ecommerce.application/Users/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using ecommerce.application.Applications.Messaging;
+using ecommerce.domain.Repositories;
+using ecommerce.domain.Shared;
+
+namespace ecommerce.application.Users.DeleteUser;
+public class DeleteUserCommandHandler 
+    : ICommandHandler<DeleteUserCommand, Guid>
+{
+    private readonly IUserRepository userRepository;
+    private readonly IUnitOfWork unitOfWork;
+    public DeleteUserCommandHandler(
+        IUserRepository userRepository,
+        IUnitOfWork unitOfWork)
+    {
+        this.userRepository = userRepository;
+        this.unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<Guid>> Handle(
+        DeleteUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        var user = await this.userRepository
+            .SelectAsync(cancellationToken, request.id);
+
+        if(user is null)
+        {
+            return Result
+                .Failure<Guid>(new Error("User.NotFound", 
+                                         $"User with id: {request.id} not found"));
+        }
+
+        this.userRepository.Delete(user);
+
+        await this.unitOfWork.SaveChangesAsync();
+
+        return request.id;
+    }
+}
