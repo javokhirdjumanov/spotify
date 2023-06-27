9bb5a2b9d5a1403faa871bcab5cf1b872ba79e29
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-27 15:06:26 +0500

    add

--- src/spotify.datalayer/EfCode/EfCoreContext.cs
+++ src/spotify.datalayer/EfCode/EfCoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using spotify.datalayer.EfClasses;
 using WEBASE.EF;
 
 namespace spotify.datalayer.EfCode
@@ -9,5 +10,19 @@ namespace spotify.datalayer.EfCode
             : base(options)
         { }
 
+        #region ENUM
+        public DbSet<OtpCodeStatus> OtpCodeStatuses { get; set; }
+        public DbSet<SessionStatus> SessionStatuses { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        #endregion
+        #region HL
+        public DbSet<OtpCode> OtpCodes { get; set; }
+        public DbSet<User> Users { get; set; }
+        #endregion
+        #region SYS
+        public DbSet<UserSession> UserSessions { get; set; }
+        #endregion
     }
 }
