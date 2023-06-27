3f7d8077d817ce9d7e2975015c602080d59ee137
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-27 15:53:35 +0500

    fixed the program.cs

--- src/spotify.api/Program.cs
+++ src/spotify.api/Program.cs
@@ -1,35 +1,30 @@
-
 using spotify.datalayer;
 
-namespace spotify.api
+namespace spotify.api;
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services
+            .AddDataLayer(builder.Configuration)
+            .AddApis(builder.Configuration);
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services
-                .AddDataLayer(builder.Configuration)
-                .AddApis(builder.Configuration);
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
