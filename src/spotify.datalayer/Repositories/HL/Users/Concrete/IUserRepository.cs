fce857e3cf1be0ce22afbdd92f6e90aae4c9b936
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-27 15:06:18 +0500

    add user repo

---
+++ src/spotify.datalayer/Repositories/HL/Users/Concrete/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using spotify.datalayer.EfClasses;
+using WEBASE.EF;
+
+namespace spotify.datalayer.Repositories;
+public interface IUserRepository
+    : IBaseEntityRepository<int, User, CreatedUserDlDto, UpdatedUserDlDto>
+{
+}
---
+++ src/spotify.datalayer/Repositories/HL/Users/Concrete/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using GenericServices;
+using spotify.datalayer.EfClasses;
+using WEBASE.EF;
+
+namespace spotify.datalayer.Repositories;
+public class UserRepository
+    : BaseEntityRepository<int, User, CreatedUserDlDto, UpdatedUserDlDto>,
+    IUserRepository
+{
+    public UserRepository(ICrudServices crudServices)
+        : base(crudServices)
+    { }
+
+
+}
