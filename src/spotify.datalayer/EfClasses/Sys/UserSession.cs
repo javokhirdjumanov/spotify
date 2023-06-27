a47aee7b1de6b423560ab32eea019664e2d4482a
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-27 12:34:35 +0500

    add sys leyare

---
+++ src/spotify.datalayer/EfClasses/Sys/UserSession.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace spotify.datalayer.EfClasses
+{
+    public class UserSession
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("token")]
+        [StringLength(255)]
+        public string Token { get; set; } = null!;
+
+        [Column("status_id")]
+        public int SessionStatusId { get; set; }
+
+        [Column("created_at", TypeName = "timestamp without time zone")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [ForeignKey("SessionStatusId")]
+        [InverseProperty("UserSessions")]
+        public virtual SessionStatus SessionStatus { get; set; } = null!;
+
+        [ForeignKey("UserId")]
+        [InverseProperty("UserSessions")]
+        public virtual User User { get; set; } = null!;
+    }
+}
