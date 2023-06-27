fe756146911ec2a34589a5bb91c104a22d79f1d1
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-27 12:37:17 +0500

    add

---
+++ src/spotify.datalayer/Dependencies.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using spotify.datalayer.EfCode;
+
+namespace spotify.datalayer
+{
+    public static class Dependencies
+    {
+        public static IServiceCollection AddDataLayer(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            services.AddDbContextPool<EfCoreContext>(options =>
+            {
+                string connectionString = configuration
+                    .GetConnectionString("DefaultConnectionString");
+
+                options.UseNpgsql(connectionString);
+            });
+
+            //AddRepositories(services);
+
+            return services;
+        }
+    }
+    //private static void AddRepositories(IServiceCollection services)
+    //{
+        
+    //}
+}
