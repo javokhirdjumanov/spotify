300f4309e482c4b6c6865d725ba627027a5e6a43
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-27 15:53:04 +0500

    add  otp code repo

---
+++ src/spotify.datalayer/Repositories/HL/OtpCodes/Concrete/OtpCodeRepository.cs
@@ -0,0 +1,15 @@
+﻿using GenericServices;
+using spotify.datalayer.EfClasses;
+using WEBASE.EF;
+
+namespace spotify.datalayer.Repositories;
+public class OtpCodeRepository
+    : BaseEntityRepository<int, OtpCode>,
+    IOtpCodeRepository
+{
+    public OtpCodeRepository(ICrudServices crudServices)
+        : base(crudServices)
+    { }
+
+
+}
