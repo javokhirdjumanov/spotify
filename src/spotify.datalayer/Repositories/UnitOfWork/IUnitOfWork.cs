6c5fb160c01ed272fb85b1875afb580b815395f7
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-27 15:05:53 +0500

    add unit of work

---
+++ src/spotify.datalayer/Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using spotify.datalayer.EfCode;
+
+namespace spotify.datalayer.Repositories;
+public interface IUnitOfWork
+{
+    EfCoreContext Context { get; }
+    IDbContextTransaction CurrentTransaction { get; }
+
+    Task<TRepository> GetRepository<TRepository>();
+    Task<IDbContextTransaction> BeginTransaction();
+    Task SaveChangesAsync();
+    Task CommitAsync();
+    Task RollbackAsync();
+}
---
+++ src/spotify.datalayer/Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using GenericServices;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using spotify.datalayer.EfCode;
+
+namespace spotify.datalayer.Repositories;
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly IServiceProvider _serviceProvider;
+    public UnitOfWork(ICrudServices crudServices, IServiceProvider serviceProvider)
+    {
+        Context = (EfCoreContext)crudServices.Context;
+        CrudServicesAsync = crudServices;
+        _serviceProvider = serviceProvider;
+    }
+    public ICrudServices CrudServicesAsync { get; }
+    public EfCoreContext Context { get; }
+    public IDbContextTransaction CurrentTransaction { get => Context.Database.CurrentTransaction; }
+
+    public Task<IDbContextTransaction> BeginTransaction()
+    {
+        return Context.Database.BeginTransactionAsync();
+    }
+    public async Task<TRepository> GetRepository<TRepository>()
+    {
+        return _serviceProvider.GetRequiredService<TRepository>();
+    }
+    public Task SaveChangesAsync()
+    {
+        return Context.SaveChangesAsync();
+    }
+    public async Task CommitAsync()
+    {
+        SaveChangesAsync();
+        if (Context.Database.CurrentTransaction != null)
+        {
+            Context.Database.CurrentTransaction.Commit();
+        }
+    }
+    public async Task RollbackAsync()
+    {
+        if (Context.Database.CurrentTransaction != null)
+        {
+            Context.Database.CurrentTransaction.Rollback();
+        }
+    }
+}
