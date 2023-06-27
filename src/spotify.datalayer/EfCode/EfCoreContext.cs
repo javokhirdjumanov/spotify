94b6e82cb7b4c8bd09715d7863c0c5691b9ecd58
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-27 12:34:47 +0500

    add db context

---
+++ src/spotify.datalayer/EfCode/EfCoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEBASE.EF;
+
+namespace spotify.datalayer.EfCode
+{
+    public partial class EfCoreContext : BaseDbContext
+    {
+        public EfCoreContext(DbContextOptions options)
+            : base(options)
+        { }
+
+    }
+}
