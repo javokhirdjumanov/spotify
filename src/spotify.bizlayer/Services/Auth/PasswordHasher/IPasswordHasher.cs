4b152a1cfd5f4113efe9311219a89a84c3f4e879
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-07-03 16:17:36 +0500

    add password hasher func

---
+++ src/spotify.bizlayer/Services/Auth/PasswordHasher/IPasswordHasher.cs
@@ -0,0 +1,6 @@
+﻿namespace spotify.bizlayer.Services;
+public interface IPasswordHasher
+{
+    string CreatePasswordHash(string password, string salt);
+    bool CheckPassword(string hashedPassword, string password, string salt);
+}
---
+++ src/spotify.bizlayer/Services/Auth/PasswordHasher/PasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace spotify.bizlayer.Services;
+public class PasswordHasher : IPasswordHasher
+{
+    private const int KeySize = 32;
+    private const int IterationCount = 1000;
+    public string CreatePasswordHash(string password, string salt)
+    {
+        using(var algorithm = new Rfc2898DeriveBytes(
+            password: password,
+            salt: Encoding.UTF8.GetBytes(salt),
+            iterations: IterationCount,
+            hashAlgorithm: HashAlgorithmName.SHA256))
+        {
+            return Convert.ToBase64String(algorithm.GetBytes(KeySize));
+        }
+    }
+    public bool CheckPassword(string hashedPassword, string password, string salt)
+    {
+        return CreatePasswordHash(password, salt)
+            .SequenceEqual(hashedPassword);
+    }
+}
