a65b1d9eadcf67e5403117a3b59566f8dbde84f9
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-07-03 09:16:35 +0500

    ..

---
+++ src/spotify.datalayer/Options/GenerateJwt/CustomClaimNames.cs
@@ -0,0 +1,8 @@
+﻿namespace spotify.datalayer.Options;
+public class CustomClaimNames
+{
+    public const string Id = "id";
+    public const string Token = "token";
+    public const string Email = "email";
+    public const string Role = "role";
+}
---
+++ src/spotify.datalayer/Options/GenerateJwt/JwtOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace spotify.datalayer.Options;
+public class JwtOptions
+{
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public string SecretKey { get; set; }
+    public int ExpirationInMinutes { get; set; }
+}
