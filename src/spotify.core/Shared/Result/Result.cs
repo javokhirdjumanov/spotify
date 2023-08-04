574d3e86440f61731f08ebf1405938de9ab5b476
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-04 15:33:40 +0500

    ..

---
+++ src/spotify.core/Shared/Result/Result.cs
@@ -0,0 +1,63 @@
+﻿namespace spotify.core.Shared;
+public class Result
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="isSuccess"></param>
+    /// <param name="error"></param>
+    /// <exception cref="InvalidOperationException"></exception>
+    protected internal Result(bool isSuccess, Error error)
+    {
+        if(isSuccess && error != Error.None) throw new InvalidOperationException();
+
+        if(!isSuccess && error == Error.None) throw new InvalidOperationException();
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public static Result Success() => new (true, Error.None);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="error"></param>
+    /// <returns></returns>
+    public static Result Failure(Error error) => new(false, error);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <param name="error"></param>
+    /// <returns></returns>
+    public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static Result<TValue> Create<TValue>(TValue? value) => value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+}
