0fd827d1bae65dc8aed99deabb0e97100b084600
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-04 15:33:35 +0500

    ..

---
+++ src/spotify.core/Errors/DomainError.cs
@@ -0,0 +1,27 @@
+﻿using spotify.core.Shared;
+
+namespace spotify.core.Errors;
+public static class DomainError
+{
+    public static class User
+    {
+        public static readonly Func<Guid, Error> NotFound = id => new Error(
+            code: "User NotFound",
+            message: $"The user with the identifier {id} was not found.");
+
+        public static readonly Func<string, string, Error> UserNotFoundByCredentials = (email, password) => new Error(
+            code: "Email or Password is not valid",
+            message: $"The user with the identifier {email},{password} was not found.");
+    }
+
+    public static class OtpCode
+    {
+        public static readonly Func<Error> InValid = () => new Error(
+            code: "Otp InValid",
+            message: "Invalid otp code");
+
+        public static readonly Func<Error> Expired = () => new Error(
+            code: "Otp.Expired",
+            message: "Expired otp code");
+    }
+}
