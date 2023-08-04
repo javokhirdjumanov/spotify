e0efd617e66c18a28189472a6fc1bdd57f3c1d5c
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-04 16:44:46 +0500

    command

---
+++ src/spotify.bizlayer/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using spotify.core.Shared;
+
+namespace spotify.bizlayer.Abstractions;
+public interface ICommand : IRequest<Result>
+{ }
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>
+{ }
