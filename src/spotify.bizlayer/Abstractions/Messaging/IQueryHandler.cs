ebae7489aa71d282ac5c837da8ecf93595e97075
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-04 16:45:07 +0500

    query handler

---
+++ src/spotify.bizlayer/Abstractions/Messaging/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using spotify.core.Shared;
+
+namespace spotify.bizlayer.Abstractions;
+public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+    where TQuery : IQuery<TResponse>
+{
+}
+
