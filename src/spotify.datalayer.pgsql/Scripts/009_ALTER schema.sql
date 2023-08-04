47ab38cc6782c4d73c6da68fbf2e8cc84133834a
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-04 15:35:04 +0500

    enum

---
+++ src/spotify.datalayer.pgsql/Scripts/009_ALTER schema.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE public.hl_user SET SCHEMA hrm;
+ALTER TABLE public.hl_otp_code SET SCHEMA hrm;
+
+ALTER TABLE public.enum_otp_code_status SET SCHEMA adm;
+ALTER TABLE public.enum_session_status SET SCHEMA adm;
+ALTER TABLE public.enum_state SET SCHEMA adm;
+ALTER TABLE public.enum_status SET SCHEMA adm;
+ALTER TABLE public.enum_user_role SET SCHEMA adm;
