401c86149ca685332fbaf54cc44e9222626b4a91
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-04 16:44:53 +0500

    command handler

---
+++ src/spotify.bizlayer/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using spotify.core.Shared;
+
+namespace spotify.bizlayer.Abstractions;
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result>
+    where TCommand : ICommand
+{ }
+
+public interface ICommandHandler<TCommand, TRespose>
+    : IRequestHandler<TCommand, Result<TRespose>>
+    where TCommand : ICommand<TRespose>
+{ }
