cbcfdc3c3f1bcfe6714ff07b8cac34d4120864aa
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-07 18:54:37 +0500

    add

---
+++ src/spotify.bizlayer/Services/OtpCodes/Validations/VerifyOtpCodeValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace spotify.bizlayer.Services.OtpCodes;
+public class VerifyOtpCodeValidator : AbstractValidator<VerifyOtpCodeCommand>
+{
+    public VerifyOtpCodeValidator()
+    {
+        RuleFor(otp => otp.OtpCode)
+            .NotNull().Empty().WithMessage("OtpCode is required")
+            .Length(6).WithMessage("OtpCode length must equel to 6");
+
+        RuleFor(otp => otp.UserId)
+                .NotNull().NotEmpty().WithMessage("UserId is required")
+                .NotEqual(default(int)).WithMessage("It does not equels default value");
+    }
+}
