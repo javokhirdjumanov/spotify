ab27d3cf1f901f9c10da8bb96c4428feadbba7dd
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-07 19:39:04 +0500

    add

---
+++ src/spotify.api/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using spotify.core.Shared;
+
+namespace spotify.api.Controllers;
+[Route("api/auth[action]")]
+[ApiController]
+public abstract class ApiController : ControllerBase
+{
+    private readonly ISender sender;
+    private readonly IServiceProvider serviceProvider;
+    protected ApiController(ISender sender, IServiceProvider serviceProvider)
+    {
+        this.sender = sender;
+        this.serviceProvider = serviceProvider;
+    }
+}
