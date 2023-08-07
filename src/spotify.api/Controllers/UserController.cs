82113f01a275b272bb12a68b2c2c4519638f3a2f
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-07 19:39:09 +0500

    add

---
+++ src/spotify.api/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace spotify.api.Controllers;
+[Route("api/user/[action]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetUserList(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
