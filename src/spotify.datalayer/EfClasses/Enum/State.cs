a8c1035cfab0559650238a7fe2b6c322293b07de
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-22 15:10:27 +0500

    ..

--- src/spotify.datalayer/EfClasses/Enum/State.cs
+++ src/spotify.datalayer/EfClasses/Enum/State.cs
@@ -1,5 +1,4 @@
-﻿using spotify.datalayer.pgsql;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using WEBASE.Models;
 
