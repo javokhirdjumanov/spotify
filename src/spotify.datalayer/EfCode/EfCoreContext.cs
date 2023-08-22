90634a36242b42643ec528ebaa32015954c03b88
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-08-22 15:10:37 +0500

    ..

--- src/spotify.datalayer/EfCode/EfCoreContext.cs
+++ src/spotify.datalayer/EfCode/EfCoreContext.cs
@@ -2,27 +2,29 @@
 using spotify.datalayer.EfClasses;
 using WEBASE.EF;
 
-namespace spotify.datalayer.EfCode
+namespace spotify.datalayer.EfCode;
+public partial class EfCoreContext : DbContext
 {
-    public partial class EfCoreContext : BaseDbContext
-    {
-        public EfCoreContext(DbContextOptions options)
-            : base(options)
-        { }
+    public EfCoreContext(DbContextOptions<EfCoreContext> options)
+        : base(options)
+    { }
 
-        #region ENUM
-        public DbSet<OtpCodeStatus> OtpCodeStatuses { get; set; }
-        public DbSet<SessionStatus> SessionStatuses { get; set; }
-        public DbSet<State> States { get; set; }
-        public DbSet<Status> Statuses { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
-        #endregion
-        #region HL
-        public DbSet<OtpCode> OtpCodes { get; set; }
-        public DbSet<User> Users { get; set; }
-        #endregion
-        #region SYS
-        public DbSet<UserSession> UserSessions { get; set; }
-        #endregion
-    }
+    #region ENUM
+    public DbSet<OtpCodeStatus> OtpCodeStatuses { get; set; }
+    public DbSet<SessionStatus> SessionStatuses { get; set; }
+    public DbSet<State> States { get; set; }
+    public DbSet<Status> Statuses { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    #endregion
+    #region HL
+    public DbSet<OtpCode> OtpCodes { get; set; }
+    public DbSet<User> Users { get; set; }
+    #endregion
+    #region SYS
+    public DbSet<UserSession> UserSessions { get; set; }
+    #endregion
+}
+public partial class EfCoreContext
+{
+
 }
