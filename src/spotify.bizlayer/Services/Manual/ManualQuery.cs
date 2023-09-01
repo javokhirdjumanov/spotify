5ac5a5a27c854d6b0297c05d7bf6049e6a9d8a6c
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-09-01 15:40:44 +0500

    ..

---
+++ src/spotify.bizlayer/Services/Manual/ManualQuery.cs
@@ -0,0 +1,8 @@
+﻿using spotify.bizlayer.Abstractions;
+
+namespace spotify.bizlayer.Services.Manual
+{
+    public record StatusQuery(int id) : IQuery<GetAllStatusResponse>;
+    public record StateQuery(int id) : IQuery<GetAllStateResponse>;
+    public record RoleQuery(int id) : IQuery<GetAllRoleResponse>;
+}
